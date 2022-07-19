86eb968fa1e116fe33bf1505b2c88e1c17e6ea33
mahdic200 <rmahdi170@gmail.com>
2022-07-19 17:18:18 +0430

    adding sql backup to project

---
+++ project 1400.04.25 20h.25min .sql
@@ -0,0 +1,332 @@
+-- phpMyAdmin SQL Dump
+-- version 5.3.0-dev+20220709.4e08d2933b
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2022 at 05:54 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `image` text COLLATE utf8_persian_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8_persian_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `image`, `url`, `created_at`, `updated_at`) VALUES
+(8, 'public/banner-image/2022-07-13-18-26-05.jpeg', 'dc', '2022-07-13 20:54:35', '2022-07-13 20:56:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(120) CHARACTER SET utf8 NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'خبری', '2022-07-13 16:19:58', NULL),
+(2, 'سیاسی', '2022-07-13 16:20:11', NULL),
+(3, 'اقتصادی', '2022-07-13 16:20:18', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `post_id` int(10) UNSIGNED NOT NULL,
+  `comment` text COLLATE utf8_persian_ci NOT NULL,
+  `status` enum('unseen','seen','approved') COLLATE utf8_persian_ci NOT NULL DEFAULT 'unseen',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `user_id`, `post_id`, `comment`, `status`, `created_at`, `updated_at`) VALUES
+(2, 3, 11, 'اره خوبه\r\n', 'approved', '2022-07-16 02:39:40', '2022-07-16 02:39:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menus`
+--
+
+CREATE TABLE `menus` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(120) COLLATE utf8_persian_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8_persian_ci NOT NULL,
+  `parent_id` int(10) UNSIGNED DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `menus`
+--
+
+INSERT INTO `menus` (`id`, `name`, `url`, `parent_id`, `created_at`, `updated_at`) VALUES
+(1, 'طراحی وب', 'link', NULL, '2022-05-05 14:25:49', NULL),
+(2, 'برنامه نویسی', 'link', NULL, '2022-05-05 14:26:17', NULL),
+(3, 'هک و امنیت', 'link', NULL, '2022-05-05 14:26:35', NULL),
+(4, 'ورزشی', 'link', NULL, '2022-07-13 18:48:18', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(120) COLLATE utf8_persian_ci NOT NULL,
+  `summary` text COLLATE utf8_persian_ci NOT NULL,
+  `body` text CHARACTER SET utf8 NOT NULL,
+  `view` int(11) NOT NULL DEFAULT 0,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `cat_id` int(11) UNSIGNED NOT NULL,
+  `image` text COLLATE utf8_persian_ci NOT NULL,
+  `status` enum('enable','disable') COLLATE utf8_persian_ci NOT NULL DEFAULT 'disable',
+  `selected` tinyint(4) NOT NULL DEFAULT 2 COMMENT 'selected => 1 \r\nnot selected => 2',
+  `breaking_news` tinyint(4) NOT NULL DEFAULT 2 COMMENT 'selected => 1\r\nnot selected => 2',
+  `users_seen` text COLLATE utf8_persian_ci DEFAULT NULL,
+  `published_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `summary`, `body`, `view`, `user_id`, `cat_id`, `image`, `status`, `selected`, `breaking_news`, `users_seen`, `published_at`, `created_at`, `updated_at`) VALUES
+(9, 'nothing', '', '', 7, 1, 1, 'public/post-image/2022-07-13-18-32-31.jpeg', 'disable', 1, 1, '4,1,3', '2022-07-13 13:28:03', '2022-07-13 20:58:25', '2022-07-16 02:13:45'),
+(10, 'new post', '', '', 8, 1, 1, 'public/post-image/2022-07-13-18-32-24.jpeg', 'disable', 1, 1, '1,3', '2022-07-13 18:32:11', '2022-07-13 21:02:24', '2022-07-16 02:13:42'),
+(11, 'mahdi post', '', '', 8, 1, 3, 'public/post-image/2022-07-13-18-32-53.jpeg', 'disable', 1, 1, '1,3', '2022-07-13 18:32:36', '2022-07-13 21:02:53', '2022-07-16 02:13:41'),
+(18, 'new post', '', '', 0, 1, 1, 'public/post-image/2022-07-16-01-28-02.png', 'disable', 2, 2, NULL, '2022-07-15 22:57:38', '2022-07-16 03:57:55', '2022-07-16 03:58:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(120) COLLATE utf8_persian_ci DEFAULT NULL,
+  `description` text COLLATE utf8_persian_ci DEFAULT NULL,
+  `keywords` text CHARACTER SET utf8 DEFAULT NULL,
+  `logo` text COLLATE utf8_persian_ci DEFAULT NULL,
+  `icon` text COLLATE utf8_persian_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `title`, `description`, `keywords`, `logo`, `icon`, `created_at`, `updated_at`) VALUES
+(1, 'سایت خبری', 'this is a test setting', 'setting , title , settings', 'link', 'link', '2022-05-05 14:24:29', '2022-07-13 18:29:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(120) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(120) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `permission` enum('user','admin') CHARACTER SET utf8 NOT NULL DEFAULT 'user',
+  `verify_token` varchar(255) COLLATE utf8_persian_ci DEFAULT NULL,
+  `is_active` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 => inactive , 1 => active',
+  `forgot_token` varchar(255) COLLATE utf8_persian_ci DEFAULT NULL,
+  `forgot_token_expire` varchar(255) COLLATE utf8_persian_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `permission`, `verify_token`, `is_active`, `forgot_token`, `forgot_token_expire`, `created_at`, `updated_at`) VALUES
+(1, 'mahdic200', 'rmahdi170@gmail.com', '$2y$10$QgDhiDUqb3s.mog9VMSMjugOLf1yvTKrKV.Kd0h0YLLW5sgBirP.W', 'admin', 'c3eb96d2b608c3ed4b9dfdff09a79b8117de838f5b7233d71975ccf5cb77f7f4', 1, NULL, NULL, '2022-07-13 17:49:47', NULL),
+(3, 'sahand', 'email@gmail.com', '$2y$10$QgDhiDUqb3s.mog9VMSMjugOLf1yvTKrKV.Kd0h0YLLW5sgBirP.W', 'admin', 'c3eb96d2b608c3ed4b9dfdff09a79b8117de838f5b7233d71975ccf5cb77f7d5', 1, NULL, NULL, '2022-07-13 17:49:47', NULL),
+(4, 'sahand', 'email2@gmail.com', '$2y$10$QgDhiDUqb3s.mog9VMSMjugOLf1yvTKrKV.Kd0h0YLLW5sgBirP.W', 'admin', 'c3eb96d2b608c3ed4b9dfdff09a79b8117de838f5b7233d71975ccf5cb77f7d5', 1, NULL, NULL, '2022-07-13 17:49:47', NULL),
+(5, 'sahand', 'email3@gmail.com', '$2y$10$QgDhiDUqb3s.mog9VMSMjugOLf1yvTKrKV.Kd0h0YLLW5sgBirP.W', 'admin', 'c3eb96d2b608c3ed4b9dfdff09a79b8117de838f5b7233d71975ccf5cb77f7d5', 1, NULL, NULL, '2022-07-13 17:49:47', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`user_id`),
+  ADD KEY `post_id_comment` (`post_id`);
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `parent_id` (`parent_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `setting`
+--
+ALTER TABLE `setting`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `post_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_id_comment` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `menus`
+--
+ALTER TABLE `menus`
+  ADD CONSTRAINT `parent_id` FOREIGN KEY (`parent_id`) REFERENCES `menus` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `cat_id` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
